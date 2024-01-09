e26943bf0a0550cb902f9c9d874999b70ddbdf3c
kamrulhaider <kamrulhaider.ssb@gmail.com>
2024-01-10 00:28:25 +0600

    Binding html

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
   const name = "Kamrul"
+  const anchor = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, asperiores!</p>'
 </script>
 
 <h1>Hello {name}</h1>
+<h2>{@html anchor}</h2>
 
 <style>
   h1{
